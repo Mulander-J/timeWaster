3f8dab5a0d24b475f9d6dc9570d450d69feeae5b
Mulander-J <mulander_j@outlook.com>
2021-03-25 14:04:05 +0800

    fix:bug

--- src/views/statistics.vue
+++ src/views/statistics.vue
@@ -17,9 +17,7 @@
       </span>
     </div>
 
-    <div
-      class="grid grid-flow-col grid-rows-2 sm:grid-rows-3 md:grid-rows-4 lg:grid-rows-5 xl:grid-rows-6 gap-4"
-    >
+    <div class="grid grid-flow-row gap-4">
       <div
         class="font-mono text-lg font-black tracking-wide flex items-center justify-around h-20 bg-indigo-400 odd:bg-purple-400 border border-gray-300 shadow rounded-md"
         v-for="i in issues"
