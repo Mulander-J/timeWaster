88839245de4347e1aa9aea95669f537234c2bbda
Mulander <39454158+Mulander-J@users.noreply.github.com>
2023-06-06 15:35:48 +0800

    Updates

---
+++ assets/app-f120e9f7.js
@@ -0,0 +1,1 @@
+import{G as s}from"./.pnpm-e78b5e43.js";import{u as e}from"./index-39833b33.js";const r=s({id:"app",state:()=>({user:null}),getters:{},actions:{getUser(){this.user=JSON.parse(e.value||"")},setUser(s){this.user=s||null,e.value=JSON.stringify(this.user)},clearUser(){this.user=null,e.value=null}}});export{r as u};
